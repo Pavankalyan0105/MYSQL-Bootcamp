6b6b9b2e62ddcfd8824f5da3d09e6f6ee4feff03
Pavankalyan0105 <pavankalyan0105@gmail.com>
2021-07-09 22:00:54 +0530

    solved real world query marks grade

--- mysql_HR/BASIC.sql
+++ mysql_HR/BASIC.sql
@@ -177,3 +177,22 @@ FROM CITY
 INNER JOIN COUNTRY
 ON CITY.CountryCode = COUNTRY.Code
 GROUP BY COUNTRY.CONTINENT;
+
+-- --------------------------------------------------------------------------------------------
+-- TODO: Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. 
+  -- The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular 
+  -- students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order.
+  --  If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+-- Write a query to help Eve.
+
+SELECT  
+CASE 
+    WHEN  Grades.Grade >=8  THEN Students.Name
+    ELSE NULL 
+    END
+, Grades.Grade , Students.Marks
+FROM Students
+INNER JOIN Grades
+ON Students.Marks between Grades.Min_Mark and Grades.Max_Mark
+ORDER BY Grades.Grade DESC, Students.Name;
